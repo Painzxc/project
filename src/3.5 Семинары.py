4a9900c3cf7c68fdf29f71914209b08b3908daea
Painzxc <zxcsingap@gmail.com>
2024-02-13 21:44:03 +0300

    1

--- src/3.5 Семинары.py
+++ src/3.5 Семинары.py
@@ -5,6 +5,15 @@ import matplotlib
 
 matplotlib.rcParams["figure.figsize"] = (13.0, 5.0)
 
+x_train = torch.rand(100)
+x_train = x_train * 20.0 - 10.0
+
+y_train = torch.sin(x_train)
+
+plt.plot(x_train.numpy(), y_train.numpy(), "o")
+plt.title("$y = sin(x)$")
+plt.show()
+
 
 # Определение класса нейронной сети SineNet
 class SineNet(torch.nn.Module):
